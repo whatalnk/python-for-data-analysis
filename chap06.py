6533fabba6d6236e27a5eff558c7d2a7b27bcc40
Wataru NAKAI <watnk0806@gmail.com>
2017-01-25 09:59:52 +0900

    Chap 6-1 Writing Data Out to Text Format

--- chap06.py
+++ chap06.py
@@ -9,18 +9,18 @@ get_ipython().magic('load_ext watermark')
 get_ipython().magic('watermark -u -d -v')
 
 
-# In[27]:
+# In[2]:
 
 import datetime
 datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
 
 
-# In[2]:
+# In[3]:
 
 get_ipython().magic('matplotlib inline')
 
 
-# In[3]:
+# In[4]:
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -129,12 +129,99 @@ result
 
 # ### Reading Text Files in Pieces
 
-# In[41]:
+# In[5]:
+
+result = pd.read_csv('pydata-book/ch06/ex6.csv')
+result
+
+
+# In[6]:
+
+pd.read_csv('pydata-book/ch06/ex6.csv', nrows=5)
+
+
+# In[12]:
+
+chunker = pd.read_csv('pydata-book/ch06/ex6.csv', chunksize=1000)
+chunker
+
+
+# In[13]:
+
+tot = Series([])
+for piece in chunker:
+    tot = tot.add(piece['key'].value_counts(), fill_value=0)
+    tot = tot.sort_values(ascending=False)
+tot
+
+
+# ### Writing Data Out to Text Format
+
+# In[14]:
+
+data = pd.read_csv('pydata-book/ch06/ex5.csv')
+data
+
+
+# In[15]:
+
+data.to_csv('out.csv')
+print(open('out.csv').read())
+
+
+# In[17]:
+
+import sys
+data.to_csv(sys.stdout, sep='|')
+
+
+# In[18]:
+
+data.to_csv(sys.stdout, na_rep='NULL')
+
+
+# In[19]:
+
+data.to_csv(sys.stdout, index=False, header=False)
+
+
+# In[21]:
+
+# data.to_csv(sys.stdout, index=False, cols=['a', 'b', 'c']) # no such arg "cols"
+data.to_csv(sys.stdout, index=False, columns=['a', 'b', 'c'])
+
+
+# In[22]:
+
+dates = pd.date_range('1/1/2000', periods=7)
+dates
+
+
+# In[23]:
+
+ts = Series(np.arange(7), index=dates)
+ts
+
+
+# In[24]:
+
+ts.to_csv('tseries.csv')
+print(open('tseries.csv').read())
+
+
+# In[25]:
+
+Series.from_csv('tseries.csv', parse_dates=True)
+
+
+# ### Manually Working with Delimited Formats
+
+# In[26]:
 
 datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
 
 
-# In[42]:
+# In[27]:
 
 imports = get_ipython().magic('imports_')
 get_ipython().magic('watermark -u -d -v -p $imports')
